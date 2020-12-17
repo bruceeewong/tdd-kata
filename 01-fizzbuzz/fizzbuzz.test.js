026ea36a45710d21067b236f8de4bde09f34326f
bruski <292803281@qq.com>
2020-12-17 23:36:43 +0800

    feat: finish fizzbuzz

--- 01-fizzbuzz/fizzbuzz.test.js
+++ 01-fizzbuzz/fizzbuzz.test.js
@@ -1,3 +1,16 @@
+const fizzbuzz = require('./fizzbuzz');
+
 describe('fizzbuzz', () => {
-  test('write down your first test case', () => {});
+  test('print Fizz when num can be divided by 3', () => {
+    expect(fizzbuzz(3)).toEqual('Fizz');
+    expect(fizzbuzz(6)).toEqual('Fizz');
+  });
+  test('print Buzz when num can be divided by 5', () => {
+    expect(fizzbuzz(5)).toEqual('Buzz');
+    expect(fizzbuzz(20)).toEqual('Buzz');
+  });
+  test('print FizzBuzz when num can be divided by both 3 and 5', () => {
+    expect(fizzbuzz(15)).toEqual('FizzBuzz');
+    expect(fizzbuzz(45)).toEqual('FizzBuzz');
+  });
 });
