19626d09c70167b81719896224cfb49dea61ae17
bruskiwang <bruskiwang@tencent.com>
2021-01-08 18:35:43 +0800

    chore: 初始化环境

--- 01-fizzbuzz/fizzbuzz.test.js
+++ 01-fizzbuzz/fizzbuzz.test.js
@@ -1,5 +1,24 @@
-const fizzbuzz = require('./fizzbuzz');
+// 01 FizzBuzz 游戏
+// 任务需求
+// ---
+// 1.打印出从1到100的数字，
+// 2.将其中3的倍数替换成“Fizz”，
+// 3.5的倍数替换成“Buzz”，
+// 4.既能被3整除、又能被5整除的数则替换成“FizzBuzz”。
 
-describe('fizzbuzz', () => {
-  test('write down your first test case', () => {});
+// 很简单对吗？以下是这道题目的其他要求：
+// ---
+// 1.代码整洁，没有重复代码
+// 2.有单元测试，单元测试覆盖率100%
+// 3.10分钟内完成
+
+// 添加 test() 方法以增加测试用例
+// 添加 expect() 方法以添加断言
+// 运行 npm run test:watch 以运行测试工具
+// 运行 npm run test:cov 以生成测试覆盖率
+
+describe("fizzbuzz", () => {
+  test("write down your first test case", () => {
+    expect(true).toEqual(true);
+  });
 });
